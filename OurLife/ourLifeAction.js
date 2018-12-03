af6918993d4d2aaeef81ece24b65a6dc0bad2b29
shixinyue <shixinyue@MyLaptop>
2018-12-03 20:17:08 +0100

    first version and upload

---
+++ OurLife/ourLifeAction.js
@@ -0,0 +1,9 @@
+function buttonFocus(id){
+	var button = document.getElementById(id);
+	button.style.backgroundColor = "white";
+}
+
+function buttonOut(id){
+	var button = document.getElementById(id);
+	button.style.backgroundColor = "transparent";
+}
