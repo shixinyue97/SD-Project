c79d4eae7ba3a1dce56820933e429caabe3e57c0
shixinyue97 <39551999+shixinyue97@users.noreply.github.com>
2019-01-14 14:47:41 +0100

    refine

--- Luebeck City/weekNews/weekNewsAction.js
+++
@@ -1,9 +0,0 @@
-function buttonFocus(id){
-	var button = document.getElementById(id);
-	button.style.color = "black";
-}
-
-function buttonOut(id){
-	var button = document.getElementById(id);
-	button.style.color = "grey";
-}
